7f985fd9b556b278ed2d9e54a4e694cc6f2751ec
Hussain fathy <Hussainfathy911@gmail.com>
2025-09-13 11:38:51 +0300

    add the logging to the file and enhance the code logic

--- prepare_member_sheet.py
+++ prepare_member_sheet.py
@@ -1,23 +1,29 @@
 import base64
+import os
+import asyncio
 from email.mime.text import MIMEText
 from googleapiclient.discovery import build
 from googleapiclient.errors import HttpError
-from final_html_body import EMAIL_HTML_BODY
+from final_html_body import EMAIL_HTML_BODY , PASS_HTML_BODY, NOT_PASS_HTML_BODY
 from config import Configuration
-from final_html_body import EMAIL_HTML_BODY
-import os
+from get_invite_link import get_invite_link
 from dotenv import load_dotenv
-
+import logging
 
 load_dotenv()
-SOCIAL_MEDIA = os.getenv('SOCIAL_MEDIA')
+SUPPORT_ASKING_LINK = os.getenv('SUPPORT_ASKING_LINK')
+SHEET_VEDEO = os.getenv('SHEET_VEDEO')
+APPLY_AGAIN_LINK = os.getenv('APPLY_AGAIN_LINK')
+GROUP_ID = os.getenv('GROUP_ID')
 
 
 class Prepare_Member_Sheet :
     
     def __init__(self):
         self.credentials = Configuration().authenticate_google()
-        self.social_media = SOCIAL_MEDIA
+        self.support_asking_link = SUPPORT_ASKING_LINK
+        self.sheet_video = SHEET_VEDEO
+        self.apply_again_link = APPLY_AGAIN_LINK
         
     def copy_and_share_sheet(self,origin_file_id, new_name, email):
         """
@@ -26,7 +32,7 @@ class Prepare_Member_Sheet :
         try:
             new_name = f"نسخة تقيم {new_name}"
             drive_service = build('drive', 'v3', credentials=self.credentials)
-            print(f"جاري إنشاء نسخة من الملف بالاسم: '{new_name}'...")
+            logging.info(f"جاري إنشاء نسخة من الملف بالاسم: '{new_name}'...")
             file_metadata = {'name': new_name}
             # نطلب ID والرابط معاً
             copied_file = drive_service.files().copy(
@@ -38,40 +44,42 @@ class Prepare_Member_Sheet :
             new_file_link = copied_file.get('webViewLink')
 
             if not new_file_id:
-                print("خطأ: فشلت عملية النسخ.")
+                logging.info("خطأ: فشلت عملية النسخ.")
                 return None, None
-            print(f"تم إنشاء النسخة بنجاح! ID: {new_file_id}")
+            logging.info(f"تم إنشاء النسخة بنجاح! ID: {new_file_id}")
 
-            print(f"جاري منح صلاحية 'محرر' للمستخدم: '{email}'...")
+            logging.info(f"جاري منح صلاحية 'محرر' للمستخدم: '{email}'...")
             user_permission = {'type': 'user', 'role': 'writer', 'emailAddress': email}
             drive_service.permissions().create(
                 fileId=new_file_id,
                 body=user_permission,
                 sendNotificationEmail=False  # --- مهم: لن نرسل الإشعار من هنا ---
             ).execute()
-            print("تم منح الصلاحية بنجاح.")
-            print(new_file_link)
+            logging.info("تم منح الصلاحية بنجاح.")
+            logging.info(new_file_link)
             return new_file_id, new_file_link
         except HttpError as error:
-            print(f'حدث خطأ في Google Drive API: {error}')
+            logging.info(f'حدث خطأ في Google Drive API: {error}')
             return None, None
         
-    def htnl_body_prepare(self,name,file_name,file_link,social_media):
+    def htnl_member_sheet(self,name,file_name,file_link,support_asking_link,sheet_video):
         
-        final_html_body = EMAIL_HTML_BODY.format(name =name ,file_name=file_name, file_link= file_link,social_media = social_media)
+        final_html_body = EMAIL_HTML_BODY.format(name =name ,file_name=file_name, file_link= file_link,
+                                                 support_asking_link = support_asking_link ,sheet_video = sheet_video)
         return final_html_body
-    def send_custom_email(self, to_email, name,file_name,file_link):
+    def send_member_sheet(self, to_email, name,file_link):
         """
         تستخدم Gmail API لإرسال بريد إلكتروني مخصص بتنسيق HTML.
         """
         try:
-            social_media = self.social_media
+            support_asking_link = self.support_asking_link
+            sheet_video = self.sheet_video
             subject = f"مرحباً بك يا{name} في مشروع 'نواة'"
             file_name = f"نسخة تقيم {name}"
             gmail_service = build('gmail', 'v1', credentials=self.credentials)
-            print(f"جاري إرسال بريد إلكتروني مخصص إلى: {to_email}...")
+            logging.info(f"جاري إرسال بريد إلكتروني مخصص إلى: {to_email}...")
         
-            message = MIMEText(self.htnl_body_prepare(name,file_name,file_link,social_media), 'html')
+            message = MIMEText(self.htnl_member_sheet(name, file_name, file_link, support_asking_link, sheet_video), 'html')
             message['to'] = to_email
             message['subject'] = subject
         
@@ -80,38 +88,48 @@ class Prepare_Member_Sheet :
             body = {'raw': raw_message}
         
             gmail_service.users().messages().send(userId='me', body=body).execute()
-            print("تم إرسال البريد الإلكتروني بنجاح!")
+            logging.info("تم إرسال البريد الإلكتروني بنجاح!")
         except HttpError as error:
-            print(f'حدث خطأ في Gmail API: {error}')
+            logging.info(f'حدث خطأ في Gmail API: {error}')
 
 
 
-
-
-# if Prepare_Member_Sheet().credentials:
-#             # 2. نسخ ومشاركة الملف باستخدام Drive API
-#             new_id, new_link = copy_and_share_sheet(
-#                 credentials, 
-#                 ORIGINAL_FILE_ID, 
-#                 NEW_FILE_NAME, 
-#                 EMAIL_TO_SHARE_WITH
-#             )
+    def htnl_pass_state(self,name,pass_state_link,pass_state):
+        if pass_state == 1 :
+            final_html_body = PASS_HTML_BODY.format(name =name ,pass_state_link = pass_state_link)
+        else:
+            final_html_body = NOT_PASS_HTML_BODY.format(name =name ,pass_state_link = pass_state_link)
+        return final_html_body
+    def send_pass_state(self, to_email, name,pass_state):
+        """
+        تستخدم Gmail API لإرسال بريد إلكتروني مخصص بتنسيق HTML.
+        """
+        try:
             
-#             # 3. إذا نجحت الخطوة السابقة، قم بإرسال الإيميل المخصص
-#             if new_id and new_link:
-#                 # تعبئة المتغيرات في رسالة الـ HTML
-#                 final_html_body = EMAIL_HTML_BODY.format(file_name=NEW_FILE_NAME, file_link= new_link)
+            if pass_state == 1 :
                 
-#                 send_custom_email(
-#                     credentials,
-#                     EMAIL_TO_SHARE_WITH,
-#                     EMAIL_SUBJECT,
-#                     final_html_body
-#                 )
+                pass_state_link =  asyncio.run(get_invite_link(GROUP_ID))
                 
-#                 print("\n" + "="*50)
-#                 print("✨ اكتملت العملية بالكامل بنجاح! ✨")
-#                 print(f"ID الملف الجديد هو:\n{new_id}")
-#                 print("="*50)
+            else:
+                pass_state_link = self.apply_again_link           
+  
+            subject = f"مرحباً بك يا{name} في مشروع 'نواة'"
+            gmail_service = build('gmail', 'v1', credentials=self.credentials)
+            logging.info(f"جاري إرسال بريد إلكتروني مخصص إلى: {to_email}...")
+        
+            message = MIMEText(self.htnl_pass_state(name,pass_state_link,pass_state), 'html')
+            message['to'] = to_email
+            message['subject'] = subject
+        
+            # 'me' تشير إلى حساب المستخدم الذي قام بالمصادقة
+            raw_message = base64.urlsafe_b64encode(message.as_bytes()).decode()
+            body = {'raw': raw_message}
+        
+            gmail_service.users().messages().send(userId='me', body=body).execute()
+            logging.info("تم إرسال البريد الإلكتروني بنجاح!")
+        except HttpError as error:
+            logging.info(f'حدث خطأ في Gmail API: {error}')
+
+
 
 
